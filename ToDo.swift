ddff737d9f780a6e4923b65f84746cf552c97246
Riddhi <rdpatel718@gmail.com>
2020-06-23 10:56:36 -0400

    Testing

---
+++ ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Riddhi Patel on 6/22/20.
+//  Copyright © 2020 Riddhi Patel. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
